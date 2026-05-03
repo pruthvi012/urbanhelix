ce5f5255fd8d546ff2f60a4888b5437cd1759670
pruthvigowda <pruthviigowda15@gmail.com>
2026-05-03 15:07:02 +0530

    chore: clean up deleted files and dev assets

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f87553f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q4nmo7qhhso"
+    "revision": "0.10g8av25vek"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
