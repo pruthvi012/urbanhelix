42a0cf5f2d1cacb39cfe57359592ddd683a308aa
pruthvigowda <pruthviigowda15@gmail.com>
2026-05-04 11:35:10 +0530

    DEBUG: Added role logging to authorize middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -47,7 +47,7 @@ const authorize = (...roles) => {
         if (!roles.includes(req.user.role)) {
             return res.status(403).json({
                 success: false,
-                message: `Role '${req.user.role}' is not authorized to access this resource`,
+                message: `DEBUG: Role '${req.user.role}' is not authorized. Allowed: ${roles.join(', ')}`,
             });
         }
         next();
