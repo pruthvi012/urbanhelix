e93ec4c6c6f7f843a73397471f9197f8e8d6b8a3
pruthvigowda <pruthviigowda15@gmail.com>
2026-08-31 18:24:02 +0530

    fix: remove express body parser to let Vercel handle it

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const connectDB = require('../server/config/db.js');
 
 const app = express();
 app.use(cors({ origin: '*' }));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+// app.use(express.json());
+// app.use(express.urlencoded());
 
 // Fix Vercel URL prefix stripping
 app.use((req, res, next) => {
@@ -27,11 +27,7 @@ app.use('/api/notifications', (req, res, next) => require('../server/routes/noti
 app.use('/api/wards', (req, res, next) => require('../server/routes/wards.js')(req, res, next));
 app.use('/api/ai', (req, res, next) => require('../server/routes/ai.js')(req, res, next));
 
-module.exports.config = {
-  api: {
-    bodyParser: false,
-  },
-};
+
 
 module.exports = async function handler(req, res) {
   try {
